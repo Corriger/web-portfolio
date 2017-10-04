f7df9b56cfae0207f46f978c90344fd51867c9c0
Corriger <rogmoraldo2@gmail.com>
2017-10-04 17:06:32 -0400

    dooone

--- src/components/Refs/index.js
+++ src/components/Refs/index.js
@@ -8,10 +8,10 @@ class Refs extends Component {
         <section>
           <p>(617)699-7450</p>
           <p><a href="mailto:http://rsmoraldo2@gmail.com">rsmoraldo2@gmail.com</a></p>
-          <p><a href="https://github.com/Corriger" target="_blank">Corriger</a></p>
+          <p><a href="https://github.com/Corriger" target="_blank" rel="noopener noreferrer">Corriger</a></p>
         </section>
         <section>
-          <a href="http://www.resilientcoders.org/" target="_blank"><img src="http://www.resilientcoders.org/240b1ae66694d9765a99dd115cf08321.png" alt="res-coders"/></a>
+          <a href="http://www.resilientcoders.org/" target="_blank" rel="noopener noreferrer"><img src="http://www.resilientcoders.org/240b1ae66694d9765a99dd115cf08321.png" alt="res-coders"/></a>
         </section>
       </div>
     )
