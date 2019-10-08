6ebd5dda5befce9180bbf8c12f41101e82251670
Roger M <rsmoraldo2@gmail.com>
2019-10-08 01:39:46 -0400

    image add

--- src/components/HeadBanner/index.js
+++ src/components/HeadBanner/index.js
@@ -23,7 +23,7 @@ const Text = styled.h1`
   color: rgba(255,215,0,1);
   font-size: 2em;
   text-align: center;
-  text-shadow: 0px 0px 4px rgba(0,0,0,0), 5px 0px 5px rgba(176,196,222,1), -5px 0px 5px rgba(176,196,222,1);
+  text-shadow: 0px 0px 4px rgba(0,0,0,1), 5px 0px 5px rgba(176,196,222,1), -5px 0px 5px rgba(176,196,222,1);
   @media only screen and (max-width: 768px){
     letter-spacing: .20em;
   }
