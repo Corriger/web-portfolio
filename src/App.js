8f5758b1ac9d99138bab3be224a6cb696e871ad4
Roger M <rsmoraldo2@gmail.com>
2019-10-11 14:33:33 -0400

    fa implement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 import HeadBanner from './components/HeadBanner'
 import NavTabs from './components/NavTabs'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from "@fortawesome/free-solid-svg-icons"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+
+library.add(
+  fab,
+  fas
+)
 
 class App extends Component {
   render() {
